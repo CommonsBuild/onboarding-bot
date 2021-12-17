509c26b5d17c5e2a7633859c730b41a9e91ba14f
Vyvy-vi <vyom.j@protonmail.com>
2021-12-17 21:33:37 +0530

    fix: message order

--- src/modules/question.ts
+++ src/modules/question.ts
@@ -60,13 +60,9 @@ export const question = async (
     collector.on("collect", async (collected) => {
       if (collected.isSelectMenu()) {
         if (collected.values[0] === "no") {
-          await collected.reply({
-            content: "Correct! Please wait...",
-            ephemeral: true,
-          });
           await interaction.editReply({
-            content: "Congrats! You will be verified shortly.",
-            components: [],
+            content: "Correct! Please wait...\nYou will be given access shortly...",
+            components: []
           });
           setTimeout(
             async () =>
@@ -82,10 +78,11 @@ export const question = async (
               "https://discord.com/channels/810180621930070088/913086515574358066/913110202218319932"
             )
             .setStyle("LINK");
-          await interaction.editReply({
+          await collected.reply({
             content:
               "For more info about the TEC, check out the <#913086515574358066> channel",
             components: [new MessageActionRow().addComponents(button)],
+            ephemeral: true,
           });
         } else {
           await interaction.editReply({
