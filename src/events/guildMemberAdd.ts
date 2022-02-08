174b672efdb5d10f1ed8df8e7b4172454bb9bc10
Vyvy-vi <vyom.j@protonmail.com>
2022-02-08 19:04:18 +0530

    chore: add license headers to all files

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,3 +1,26 @@
+/*
+ * This file is part of the tec-onboarding-bot project
+ * The contents are derived from the commit-your-code-bot project
+ *
+ * Copyright (c) 2021 nhcarigan
+ * Authors: Nicholas Carrigan
+ * Modified by: Vyvy-vi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import { GuildMember } from "discord.js";
 
 import { logHandler } from "../utils/logHandler";
